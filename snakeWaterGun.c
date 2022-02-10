aefeaf83f25f7bcee90d79b7035b4e4645435c23
veenu3 <goyalveenu3@gmail.com>
2022-02-10 12:15:28 +0530

    add code

--- snakeWaterGun.c
+++ snakeWaterGun.c
@@ -4,7 +4,7 @@
 int main()
 {
     char you, comp;
-    int n,comp_win =0,user_win =0;
+    int n,round=1,comp_win =0,user_win =0;
     srand(time(0));
     int number = rand() % 100 + 1;
 
@@ -21,49 +21,64 @@ int main()
         comp = 'g';
     }
     printf("Snake - Water - Gun\n");
-    printf("Snake -'s'\nWater -'w'\nGun -'g'\n");
-    printf("Choose your option:");
-    scanf("\n%c", &you);
-    if (you == comp) {
-        comp_win && user_win ==0;
-    }
-    if (you == 's' && comp == 'g')
-    {
-        comp_win  +=1;
-    }
-    else if (you == 'g' && comp == 's')
-    {
-        user_win +=1;
-    }
-
-    if (you == 's' && comp == 'w')
-    {
-        user_win +=1;
-    }
-    else if (you == 'w' && comp == 's')
-    {
-        comp_win +=1;
-    }
-
-    if (you == 'g' && comp == 'w')
-    {
-        comp_win +=1;
-    }
-    else if (you == 'w' && comp == 'g')
-    {
-        user_win +=1;
-    }
-
-    if (user_win > comp_win){
-        printf("You Won round%d \n");
+    printf("Enter number of rounds: ");
+    scanf("%d", &n);
+    while(round <= n ){
+        printf("Round%d\n",round);
+        printf("Snake -'s'\nWater -'w'\nGun -'g'\n");
+        printf("Choose your option:");
+        scanf("\n%c", &you);
+        if (you == comp) {
+            comp_win && user_win ==0;
         }
-    else if (comp_win > user_win){
-        printf("Computer Won round %d \n");
+        if (you == 's' && comp == 'g')
+        {
+            comp_win  +=1;
         }
-    else{
-        printf("Draw!!\n");
+        else if (you == 'g' && comp == 's')
+        {
+            user_win +=1;
         }
-    printf("You chose %c and computer chose %c.\n ", you,comp);
+
+        if (you == 's' && comp == 'w')
+        {
+            user_win +=1;
+        }
+        else if (you == 'w' && comp == 's')
+        {
+            comp_win +=1;
+        }
+
+        if (you == 'g' && comp == 'w')
+        {
+            comp_win +=1;
+        }
+        else if (you == 'w' && comp == 'g')
+        {
+            user_win +=1;
+        }
+
+        if (user_win > comp_win){
+            printf("You Won round%d \n",round);
+         }
+        else if (comp_win > user_win){
+            printf("Computer Won round %d \n",round);
+         }
+        else{
+            printf("Draw!!\n");
+        }
+        printf("You chose %c and computer chose %c.\n ", you,comp);
+        round += 1;
+    }
+if (user_win > comp_win){
+    printf("\nCongratulations!! You Won");
+    }
+else if (comp_win > user_win){
+    printf("\nYou lose!!");
+    }
+else{
+    printf("\nMatch Draw!!");
+    }
 return 0;
 }
 
