97c7a0184c085b4f1876b1719c468299d3016f13
veenu3 <goyalveenu3@gmail.com>
2022-02-21 11:22:13 +0530

    add code

--- snakeWaterGun.c
+++ snakeWaterGun.c
@@ -1,65 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main()
-{
-    char you, comp;
-    int n,round=1,comp_win =0,user_win =0;
+int main(){
+    char you, comp,ch;;
+    int n,round,globalcompscore=0,globaluserscore=0;
     srand(time(0));
+    do{
+    round=1;
     int number = rand() % 100 + 1;
-
-    if (number < 33)
-    {
+    if (number < 33){
         comp = 's';
     }
-    else if (number > 33 && number < 66)
-    {
+    else if (number > 33 && number < 66){
         comp = 'w';
     }
-    else
-    {
+    else{
         comp = 'g';
     }
-    printf("Snake - Water - Gun\n");
+    printf("Welcome To Snake,Water,Gun Game\n");
     printf("Enter number of rounds: ");
     scanf("%d", &n);
     while(round <= n ){
-        printf("Round%d\n",round);
-        printf("Snake -'s'\nWater -'w'\nGun -'g'\n");
+        int comp_win = 0,user_win=0;
+        printf("\nRound%d",round);
+        printf("\nSnake -'s'\nWater -'w'\nGun -'g'\n");
         printf("Choose your option:");
         scanf("\n%c", &you);
-        if (you == comp) {
-            comp_win && user_win ==0;
+        if(you!= 's' && you != 'w' && you != 'g'){
+          printf("Invalid input, try again\n");
+            continue;
         }
-        if (you == 's' && comp == 'g')
-        {
-            comp_win  +=1;
+        if (comp == 's') {
+            if (you == 'w') {
+                comp_win = 1;
+            }
+            else if(you == 'g') {
+                user_win = 1;
+            }
         }
-        else if (you == 'g' && comp == 's')
-        {
-            user_win +=1;
+        else if(comp == 'w') {
+            if (you == 'g') {
+                comp_win = 1;
+            }
+            else if (you == 's') {
+                user_win = 1;
+            }
         }
-
-        if (you == 's' && comp == 'w')
-        {
-            user_win +=1;
+        else if(comp == 'g') {
+            if (you== 's') {
+                comp_win = 1;
+            }
+            else if (you == 'w') {
+                user_win = 1;
+            }
         }
-        else if (you == 'w' && comp == 's')
-        {
-            comp_win +=1;
-        }
-
-        if (you == 'g' && comp == 'w')
-        {
-            comp_win +=1;
-        }
-        else if (you == 'w' && comp == 'g')
-        {
-            user_win +=1;
-        }
-
         if (user_win > comp_win){
-            printf("You Won round%d \n",round);
+            printf("You Won round %d \n",round);
          }
         else if (comp_win > user_win){
             printf("Computer Won round %d \n",round);
@@ -68,17 +64,25 @@ int main()
             printf("Draw!!\n");
         }
         printf("You chose %c and computer chose %c.\n ", you,comp);
+        globalcompscore+=comp_win;
+        globaluserscore+=user_win;
         round += 1;
     }
-if (user_win > comp_win){
+if (globaluserscore > globalcompscore){
     printf("\nCongratulations!! You Won");
     }
-else if (comp_win > user_win){
+else if (globalcompscore > globaluserscore){
     printf("\nYou lose!!");
     }
 else{
     printf("\nMatch Draw!!");
     }
+printf("\nDo you want to continue \n");
+printf("press :y for 'yes'\n");
+printf("press :n for 'no'\n");
+scanf(" %c",&ch);
+}while (ch=='y');
+printf("THNAK YOU!");
 return 0;
 }
 
